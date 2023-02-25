296cd7a804737d6a0fad255926c78d2db3212f66
Kevin Peter <kevinrinaldopeter713@gmail.com>
2023-02-25 20:08:38 +0530

    Header updated with logo

--- src/pages/PageShell/Header.tsx
+++ src/pages/PageShell/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles/Header.scss';
-// import log from '../../assets/persistentLogo.svg';
+import logo from '../../assets/508.png';
 import log from '../../assets/PSL_Logo.png';
 function Header() {
   return (
@@ -8,23 +8,30 @@ function Header() {
        <div className="container-fluid">
         <div className="navbar-header">
           <a className="navbar-brand" href="#">
-            <img src={log} alt="Logo" width='35' height='35' />
+            <img src={log} alt="Logo" width='35' height='35' style={{marginRight:12,marginLeft:-12}}/>
             <span ></span>
-            <span ></span>
-            <span ></span>
-            <span ></span>
-            <span ></span>
-            <span ></span>
-            <span ></span>
-            <span ></span>
-            <span ></span>
+            {/* <span ></span>
+            <span ></span> */}
+            {/* <span ></span> */}
             <span >FiveO8</span>
-            {/* <img src={logo} alt="Logo" width='80' height='34'/> */}
+            <span ></span>
+            {/* <span ></span>
+            <span ></span> */}
+          <span ></span>
+          <span ></span>
+            {/* <span >FiveO8</span> */}
+            <img src={logo} alt="Logo" width='70' height='32'/>
            
           </a>
         </div>
         <div className="nav navbar-nav navbar-right">
-          Log in
+        <button
+              type="button"
+              className="btn"
+              
+            >
+             Sign up
+            </button> 
         </div>
        </div>
      </nav>
